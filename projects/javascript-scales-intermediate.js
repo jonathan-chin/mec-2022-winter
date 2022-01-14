cb1b7b2d680ca06ae86903fcf36899d3404f1ea1
Jon Chin <jon@sharemeals.org>
2022-01-14 10:02:02 -0500

    fixed errors

--- projects/javascript-scales-intermediate.js
+++ projects/javascript-scales-intermediate.js
@@ -1,7 +1,3 @@
-
-
-
-
 /**
  * @param {array<number>} numbers - an array of numbers
  * @constraint implement this using built in Array functions; no for or while loops;
@@ -21,38 +17,6 @@ function filter_out_negative_values(numbers){
 function add_all_negative_values(numbers){
 
 }
-
-
-/**
- * @param {string} name - someone's first name
- * @constraint use the https module and the api here: https://agify.io/
- * @return {number} the predicted age of that person
- **/
-
-async function predict_age_by_name(name){
-    
-}
-
-/**
- * @param {string} artist - the name of a musician
- * @return {array<string>} an array of song titles by the given artist
- **/
-
-async function list_songs_by(artist){
-
-    return []; // replace with yours
-}
-
-/**
- * ----------------
-   ----------------
-   DON'T CHANGE ANYTHING BELOW THIS LINE
-   ----------------
-   ----------------
- *
- **/
-
-// THIS IS A CHALLENGE
 
 /**
  * @param {date_one} Date - a random date
@@ -74,12 +38,41 @@ function validate_uuid(uuid){
 
 }
 
+/**
+ * @param {string} name - someone's first name
+ * @constraint use the axios module and the api here: https://agify.io/
+ * @return {number} the predicted age of that person
+ **/
+
+async function predict_age_by_name(name){
+    
+}
+
+/**
+ * @param {string} artist - the name of a musician
+ * @constraint use the axios module and the api here: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+ * @return {array<string>} an array of song titles by the given artist
+ **/
+
+async function list_songs_by(artist){
+
+    return []; // replace with yours
+}
+
+/**
+ * ----------------
+   ----------------
+   DON'T CHANGE ANYTHING BELOW THIS LINE
+   ----------------
+   ----------------
+ *
+ **/
+
 const commands = [
     'days_between(new Date("June 14, 1989"), new Date("October 26, 2005"))',
     'validate_uuid("710b962e-041c-11e1-9234-0123456789ab")',
     'filter_out_negative_values([-84, 99, 42, -71, 54, -94, -26, 91, 13, 100, 17, -21])',
-    'add_all_negative_values([-84, 99, 42, -71, 54, -94, -26, 91, 13, 100, 17, -21])',
-    'predict_age_by_name("Christina")'
+    'add_all_negative_values([-84, 99, 42, -71, 54, -94, -26, 91, 13, 100, 17, -21])'
 ];
 
 for(const command of commands){
@@ -90,3 +83,5 @@ async function run_async_functions(){
     console.log('predict_age_by_name("Christina") = ' + await predict_age_by_name("Christina"));
     console.log('list_songs_by("Beastie Boys") = ' + (await list_songs_by('Beastie Boys')).join(', '));
 }
+
+run_async_functions();
