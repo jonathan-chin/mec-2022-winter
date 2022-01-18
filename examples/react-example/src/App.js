9fcab66ef2382755d0d17ca45fe28d182b32ac31
Jon Chin <jon@sharemeals.org>
2022-01-18 11:14:50 -0500

    added title and prep time

--- examples/react-example/src/App.js
+++ examples/react-example/src/App.js
@@ -12,6 +12,8 @@ import './App.css';
 // React components are JS functions that return HTML / other components
 // React component names should be capitalized
 function App() {
+    const title_1 = 'Ramen';
+    const prep_time_1 = '20 min';
     const ingredients_1 = [
 	'some ramen noodles',
 	'1 cup of water',
@@ -19,6 +21,8 @@ function App() {
 	'2 eggs'
     ];
 
+    const title_2 = 'Curry Mutton';
+    const prep_time_2 = '40 min';
     const ingredients_2 = [
 	'scallions',
 	'curry powder',
@@ -30,8 +34,11 @@ function App() {
     // note: for React, the class attribute must be named className instead
     return (
 	<div className='recipe-parent'>
-	    <Ingredients ingredients={ingredients_1} />
-	    <Ingredients ingredients={ingredients_2} />
+	    <Ingredients ingredients={ingredients_1} title={title_1} prep_time={prep_time_1} />
+	    {/* to pass JS variables to components, include them as an attribute.
+		for example: attribute_name={variable_name}
+	      */}
+	    <Ingredients ingredients={ingredients_2} title={title_2} prep_time={prep_time_2} />
 	</div>
     );
 }
