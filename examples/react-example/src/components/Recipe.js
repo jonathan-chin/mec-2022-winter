af208e2ed2fb646d3d63d1cfec500a6658518ca5
Jon Chin <jon@sharemeals.org>
2022-01-19 14:56:33 -0500

    added upvote demo

--- examples/react-example/src/components/Recipe.js
+++ examples/react-example/src/components/Recipe.js
@@ -1,7 +1,12 @@
-import Rating from '@mui/material/Rating';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+
+// to use state variables, we need to first import the useState hook from react.
+// with hooks, we cannot call them as components, ie: <useState />
+import {useState} from 'react';
 
 // to create a React component, use a JS function that returns some HTML / other components
 // React component names should be capitalized
@@ -13,11 +18,18 @@ function Recipe({
     alt_text,
     image
 }){
-    
     /*
-       <li>ingredient 1</li>
-       <li>ingredient 2</li>
+       To allow a component to update or rerender itself with new information, we have to use state variables.
      */
+    /*
+       useState syntax:
+       let [name_of_variable, name_of_function_to_change_variable] = useState(default_value_for_variable);
+    */
+    let [upvotes, setUpvotes] = useState(0);
+
+    const handleUpvoteClick = () => {
+	setUpvotes(upvotes + 1);
+    };
     
     return (
 	<Card>
@@ -29,8 +41,16 @@ function Recipe({
 	    />
 	    <CardContent>
 		<h2>
+		    {/* sometimes, we want to do a conditional render */}
+		    {upvotes >= 25 && <span>hot! </span>}
 		    {title}
 		</h2>
+		<div>
+		    {upvotes}
+		    <IconButton aria-label="upvote" onClick={handleUpvoteClick}>
+			<ArrowCircleUpIcon />
+		    </IconButton>
+		</div>
 		<p>
 		    {prep_time}
 		</p>
@@ -43,7 +63,6 @@ function Recipe({
 			}
 		    )}
 		</ol>
-		<Rating name="no-value" value={null} />
 	    </CardContent>
 	</Card>
     );
